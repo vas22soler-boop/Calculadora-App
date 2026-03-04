13ad08e128912884c7a8504ed9aaa6bf3a9dbd4c
LeonardoA <vas22soler@gmail.com>
2026-03-04 11:42:29 -0300

    Criando Botoes

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
@@ -7,38 +7,111 @@ export default function App() {
       <StatusBar style="light" />
 
       <View style={styles.displayContainer}>
-        <Text style={styles.displayText} numberOflines={1}> 1000
-        </Text>
+          <Text style={styles.displayText} numberOfLines={1}>
+              1000
+          </Text>
       </View>
-    </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style = {styles.functionText}>C</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.functionButton}>
+          <Text style = {styles.functionText}>+/-</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.functionButton}>
+          <Text style = {styles.functionText}>%</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.operatorButton}>
+          <Text style = {styles.operatorText}>÷</Text>
+        </TouchableOpacity>
+      </View> 
+      <View style={styles.row}></View>
+        <TouchableOpacity style={styles.buttonText}>
+          <Text style = {styles.buttonText}>7</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.buttonText}>
+          <Text style = {styles.buttonText}>8</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.buttonText}>
+          <Text style = {styles.buttonText}>9</Text>
+        </TouchableOpacity>
+         <TouchableOpacity style={styles.operatorButton}>
+          <Text style = {styles.operatorText}>X</Text>
+        </TouchableOpacity>
+      </View>
+
+      
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000000',
   },
-  displayContainer:{
+  displayContainer: {
     flex: 2.5,
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
     alignItems: 'flex-end',
     paddingHorizontal: 20,
-    paddingBottom: 20,
+    paddingBottom: 20
   },
-  displayText:{
+  displayText: {
     color: '#FFFFFF',
     fontSize: 72,
     fontWeight: '300',
   },
   row: {
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
-
+    justifyContent: 'space-between',
+    paddingHorizontal: 10
   },
+  operatorButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height:'75%',
+    backgroundColor: '#FF9500',
   },
+   functionButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height:'75%',
+    backgroundColor: '#A5A5A5',
+  },
+  operatorText: {
+    color: '#fff',
+    fontSize:36,
+  },
+  functionText: {
+    color: '#000',
+    fontSize:28,
+  },
+  buttonNumber: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height:'75%',
+    backgroundColor: '#333333',
+  },
+  buttonText: {
+    color: 'fff',
+    fontSize:32,
+  },
+  doubleWidthButton: {
+    flex: 2,
+    alignItems:'flex-start',
+    paddingLeft: 32,
+  }
+}
 );
